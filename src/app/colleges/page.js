4287b16222f6f6d8f4940b8220a5c6abf3681440
Yasaswini <yasaswinireddy16@gmail.com>
2026-06-22 21:44:32 +0530

    Fixed deployment issue

--- src/app/colleges/page.js
+++ src/app/colleges/page.js
@@ -1,16 +1,7 @@
 import SearchableCollegeList from "@/components/SearchableCollegeList";
+import colleges from "@/data/colleges.json";
 
-async function getColleges() {
-  const res = await fetch("http://localhost:3000/api/colleges", {
-    cache: "no-store",
-  });
-
-  return res.json();
-}
-
-export default async function CollegesPage() {
-  const colleges = await getColleges();
-
+export default function CollegesPage() {
   return (
     <div className="p-10 bg-gray-100 min-h-screen">
       <h1 className="text-4xl font-bold mb-8 text-center">
